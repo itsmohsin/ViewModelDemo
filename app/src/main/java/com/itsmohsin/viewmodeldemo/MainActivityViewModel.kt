b3a69b7fe6ca2ea355ad15457e892e161fceb927
itsmohsin <mohsinsmannat@gmail.com>
2022-08-25 19:07:13 +0530

    ViewModel added to counter

---
+++ app/src/main/java/com/itsmohsin/viewmodeldemo/MainActivityViewModel.kt
@@ -0,0 +1,12 @@
+package com.itsmohsin.viewmodeldemo
+
+import androidx.lifecycle.ViewModel
+
+class MainActivityViewModel : ViewModel() {
+    var count = 0
+
+    fun updateCount(){
+        ++count
+    }
+
+}
